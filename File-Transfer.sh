29d7a1987c76fae1dcaf6d2dc9d07db082981392
James Ingram <smurfjames@gmail.com>
2020-02-01 21:42:29 +0000

    Added Directory Pull Decryption
    Added Push Option PID Folder Creation
    Corrected Renaming Function Filter
    Added Send Object Encryption
    Temporary Removal Of Email Function

--- File-Transfer.sh
+++ File-Transfer.sh
@@ -3,7 +3,7 @@
 mkdir $$
 touch $$/email.txt
 echo "Subject: Delivery Notification">> $$/email.txt
-if [ ${12} == "Full" ] || [ ${12} == "Pull" ]; then
+if [ ${13} == "Full" ] || [ ${13} == "Pull" ]; then
     source_user="$1"
     source_host="$2"
     source_password="$3"
@@ -11,7 +11,6 @@ if [ ${12} == "Full" ] || [ ${12} == "Pull" ]; then
     decryption_password="$5"
     decrypted=`echo $source_password | openssl enc -aes-256-cbc -a -d -salt -pass pass:"ntSQ8gHt)-DXX9u\!"`
     decoded=`echo $decrypted | base64 --decode`
-
     export SSHPASS=$decoded
     # Get Directory
     if [ ${11} == "Dir" ]; then 
@@ -20,6 +19,7 @@ if [ ${12} == "Full" ] || [ ${12} == "Pull" ]; then
             get -R $remote_object_name 
             bye
 !
+        gpg --yes --batch -q --multifile --passphrase="$decryption_password" --decrypt $$/$remote_object_name*.gpg
         echo "Completed Directory Collection" 
     # Get Plaintext File
     elif [ ${11} == "File" ] || [ ${11} == "Zip" ]; then
@@ -42,20 +42,29 @@ if [ ${12} == "Full" ] || [ ${12} == "Pull" ]; then
 fi
 
 # Connect To Destination Server
-if [ ${12} == "Full" ] || [ ${12} == "Push" ]; then
+if [ ${13} == "Full" ] || [ ${13} == "Push" ]; then
+    if [ ! -d "$$" ]; then
+        mkdir $$
+    fi
     target_user="$6"
     target_host="$7"
     target_password="$8"
     local_object_name="$9"
-    encryption_password="${10}"
+    recipient="${10}"
     decrypted=`echo $target_password | openssl enc -aes-256-cbc -a -d -salt -pass pass:"ntSQ8gHt)-DXX9u\!"`
     decoded=`echo $decrypted | base64 --decode`
     export SSHPASS=$decoded
-    if [ $4 != $9 ] && [ $9 != *".gpg"*] && [ $4 != *".gpg"*]; then
+    if [ $4 != $9 ] && [[ $9 != *".gpg"* ]] && [[ $4 != *".gpg"* ]]; then
         mv $$/$remote_object_name $$/$local_object_name # Renaming
     fi 
     # Send Directory
     if [ ${11} == "Dir" ]; then
+        if [ ${12} == "sendEnc" ]; then
+            for nested_file in $$/$local_object_name/*
+            do
+                gpg --yes --batch -q --output ${nested_file}.gpg --recipient="$recipient" -e $nested_file
+            done
+        fi
         sshpass -e sftp -q $target_user@$target_host << ! 
         put -r $$/$local_object_name
         bye
@@ -64,25 +73,35 @@ if [ ${12} == "Full" ] || [ ${12} == "Push" ]; then
         echo "Successfully Delivered $local_object_name" >> $$/email.txt 
     # Send Plaintext File
     elif [ ${11} == "File" ]; then
-        gpg --yes --batch -q --passphrase="$encryption_password" -c $$/$local_object_name
+        if [ ${12} == "sendEnc" ]; then
+            gpg --yes --batch -q --output $$/${local_object_name}.gpg --recipient="$recipient" --encrypt $$/$local_object_name
+            local_object_name=$local_object_name.gpg
+        fi
         sshpass -e sftp -q $target_user@$target_host << ! 
-        put $$/${local_object_name}.gpg
+        put $$/$local_object_name
         bye
 !
         echo "Completed Plaintext File Transfer" 
         echo "Successfully Delivered $local_object_name" >> $$/email.txt 
     # Send Encrypted File
-    
     elif [ ${11} == "EncF" ]; then 
-        gpg --yes --batch -q --passphrase="$encryption_password" -c $$/$local_object_name
-        mv $$/$local_object_name.gpg $$/$local_object_name # Rename .gpg.gpg To .gpg
+        if [ ${12} == "sendEnc" ]; then
+            gpg --yes --batch -q --output $$/$local_object_name --recipient="$recipient" --encrypt $$/$local_object_name
+        fi
         sshpass -e sftp -q $target_user@$target_host << ! 
             put $$/$local_object_name
             bye
 !
         echo "Completed Encrypted File Transfer" 
+        echo "Successfully Delivered $local_object_name" >> $$/email.txt
     elif [ ${11} == "Zip" ]; then
         unzip $$/$local_object_name -d $$/
+        if [ ${12} == "sendEnc" ]; then
+            for nested_file in $$/${local_object_name%.zip}/*
+            do
+                gpg --yes --batch -q --recipient="$recipient" -e $nested_file
+            done
+        fi
         sshpass -e sftp -q $target_user@$target_host << !
             put -r $$/${local_object_name%.zip}
             bye
@@ -90,7 +109,6 @@ if [ ${12} == "Full" ] || [ ${12} == "Push" ]; then
         echo "Completed Zip Archive Transfer"
         echo "Successfully Delivered $local_object_name" >> $$/email.txt 
     fi
+    rm -rf $$
 fi
-#cat $$/email.txt | ssmtp rajvir.bunet01@dixonscarphone.com
-cat $$/email.txt | ssmtp james.ingram01@dixonscarphone.com
-rm -rf $$
+#cat $$/email.txt | ssmtp james.ingram01@dixonscarphone.com
