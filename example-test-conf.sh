5d53a9358279f03742aec4a991a9ab9ab544fe39
James-Ingram <smurfjames@gmail.com>
2020-05-18 00:22:08 +0100

    Added Clarification To Example Conf

--- example-test-conf.sh
+++ example-test-conf.sh
@@ -1,7 +1,9 @@
 # Anything not used should be "."
-# Who do you PULL as?
+# What was the salt used when generating the password (Required)
+export salt=""
+# Who do you PULL as? (Username for Source server)
 export source_user=""  
-# Where do you PULL to?
+# Where do you PULL to? (IP/url for Source server)
 export source_host=""
 # What is the password to do so? (AES-256 Encrypt the base64 version of the plaintext, using the salt in the Main script)
 export source_password="" 
@@ -9,15 +11,15 @@ export source_password=""
 export remote_object_name=""
 # If encrypted, What is the password for it?
 export decryption_password=""
-# Who do you PUSH as?
+# Who do you PUSH as? (Username for destination server)
 export target_user=""
-# Where do you PUSH to?
+# Where do you PUSH to? (IP/url for Destination server)
 export target_host=""
 # What is the password to do so? (AES-256 Encrypt the base64 version of the plaintext, using the salt in the Main script)
 export target_password=""
 # What should the file pushed be called? (Used in renaming, and encrypted files (this should include ".gpg"))
 export local_object_name=""
-# Who to email when the action completes? (Emailed everytime...)
+# Who to email when the action completes? (Emailed everytime... You were Warned)
 export recipient=""
 # What type of object is being moved? (Options: File, EncF, Dir, Zip)
 export object_type=""
